4f95c5e50ae602d8f400d6837f62f03a3b2727d9
pacificyost <pacificyost@gmail.com>
2018-11-03 11:22:55 -0700

    Background Music
    Added background music audio source that will load on start.

--- Squeak/Assets/Scripts/AudioManager.cs
+++ Squeak/Assets/Scripts/AudioManager.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour {
+    
+    public AudioSource BGM;
+    private AudioSource BGMInstance;
 
 	// Use this for initialization
 	void Start () {
         DontDestroyOnLoad(gameObject);
+
+        BGMInstance = GameObject.Instantiate(BGM, this.transform);
+        StartMusic();
 	}
+
+    public void StartMusic()
+    {
+        BGMInstance.Play();
+    }
 	
 	
 }
