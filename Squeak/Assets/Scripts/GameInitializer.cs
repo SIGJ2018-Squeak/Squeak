80a13dbfe049c557e884b4e957ae3d6efd696496
pacificyost <pacificyost@gmail.com>
2018-11-03 10:38:34 -0700

    More stubs
    Created game initializer and audio manager stubs

---
+++ Squeak/Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInitializer : MonoBehaviour {
+
+    public GameObject audioManager;
+
+    void Awake () {
+        if (GameObject.FindObjectOfType<AudioManager>() == null)
+        {
+            GameObject.Instantiate(audioManager);
+        }
+	}
+	
+	
+}
