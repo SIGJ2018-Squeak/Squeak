c9e2c3cf1ffa9259080595bc2f5201e736394c3b
badfade <badfadestudios@gmail.com>
2018-11-03 16:38:52 -0700

    Basic 3D Scene

--- Squeak/Assets/Scripts/CameraController.cs
+++ Squeak/Assets/Scripts/CameraController.cs
@@ -6,15 +6,16 @@ public class CameraController : MonoBehaviour {
     public GameObject player;
     public float followAhead;
     public float followAbove;
-
+   
     public float smoothing;
 
     private Vector3 _targetPosition;
+    private float zPosition;
 
     // Use this for initialization
     void Start()
     {
-
+        zPosition = transform.position.z;
     }
 
     // Update is called once per frame
@@ -22,11 +23,11 @@ public class CameraController : MonoBehaviour {
     {
         if (player.transform.localScale.x > 0f)
         {
-            _targetPosition = new Vector3(player.transform.position.x + followAhead, player.transform.position.y + followAbove, -10f);
+            _targetPosition = new Vector3(player.transform.position.x + followAhead, player.transform.position.y + followAbove, zPosition);
         }
         else
         {
-            _targetPosition = new Vector3(player.transform.position.x - followAhead, player.transform.position.y + followAbove, -10f);
+            _targetPosition = new Vector3(player.transform.position.x - followAhead, player.transform.position.y + followAbove, zPosition);
         }
 
         transform.position = Vector3.Lerp(transform.position, _targetPosition, smoothing * Time.deltaTime);
