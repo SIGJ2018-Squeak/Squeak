5f38b5002f5b66a4dacadcc1c99a1f1783948739
pacificyost <pacificyost@gmail.com>
2018-11-03 14:24:51 -0700

    Added restart timer to end scene

---
+++ Squeak/Assets/Scripts/RestartTimer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartTimer : MonoBehaviour {
+
+    public float restartDelay = 15.0f;
+
+
+	// Use this for initialization
+	void Start () {
+        Invoke("GoToStart", restartDelay);
+	}
+	
+    void GoToStart()
+    {
+        SceneLoader.GoToScene(0);
+    }
+}
