887fbf1ded776ee0fc592687e963875598c8f82e
pacificyost <pacificyost@gmail.com>
2018-11-03 15:46:54 -0700

    Cheese
    Added a cheese pickup that ends the game.

---
+++ Squeak/Assets/Scripts/Cheese.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cheese : MonoBehaviour {
+
+    private AudioSource sound;
+
+    private void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        sound.Play();
+        SceneLoader.GoToNextScene();
+        
+    }
+}
