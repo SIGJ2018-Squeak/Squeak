22f78f5c4a7aeee5324df3388de2a6f3828396b8
pacificyost <pacificyost@gmail.com>
2018-11-03 15:34:01 -0700

    Scene timeouts
    Added scene timeouts to move through the title and help scenes.

--- Squeak/Assets/Scripts/GameInitializer.cs
+++ Squeak/Assets/Scripts/GameInitializer.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class GameInitializer : MonoBehaviour {
 
     public GameObject audioManager;
+    public float titleTimeout = 10.0f;
 
     void Awake () {
         if (GameObject.FindObjectOfType<AudioManager>() == null)
         {
             GameObject.Instantiate(audioManager);
         }
+        Invoke("GoToNextScene", titleTimeout);
 	}
 	
-	
+	void GoToNextScene()
+    {
+        SceneLoader.GoToNextScene();
+    }
 }
---
+++ Squeak/Assets/Scripts/HelpTimeout.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelpTimeout : MonoBehaviour {
+    public float helpTimeout = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+
+        Invoke("GoToNextScene", helpTimeout);
+    }
+
+    void GoToNextScene()
+    {
+        SceneLoader.GoToNextScene();
+    }
+
+}
