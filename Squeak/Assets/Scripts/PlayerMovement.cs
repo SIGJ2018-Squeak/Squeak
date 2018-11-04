b059d7a0b289752d60aca671d732596de1a0d5b1
pacificyost <pacificyost@gmail.com>
2018-11-04 12:27:34 -0800

    Sonic particle effect

--- Squeak/Assets/Scripts/PlayerMovement.cs
+++ Squeak/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     public float burstSpeed;
     public bool burstAvailable = false;
     private float _horizontalVelocity;
+    private ParticleSystem sonicParticle;
 
     public float jumpSpeed;
     private float _verticalVelocity;
@@ -41,6 +42,7 @@ public class PlayerMovement : MonoBehaviour
         _facingY = -1f;
 
         _animator = GetComponent<Animator>();
+        sonicParticle = GetComponentInChildren<ParticleSystem>();
     }
 
     // Update is called once per frame
@@ -57,8 +59,19 @@ public class PlayerMovement : MonoBehaviour
                 {
                     AudioSource.PlayClipAtPoint(sonicSqueak, transform.position);
                 }
+                if (sonicParticle != null)
+                {
+                    if (_facingX > 0)
+                    {
+                        sonicParticle.gameObject.transform.localScale = new Vector3(1f, 1f, 1f);
+                    }
+                    else
+                    {
+                        sonicParticle.gameObject.transform.localScale = new Vector3(-1f, -1f, -1f);
+                    }
+                    sonicParticle.Play();
+                }
                 SpeedBurst();
-
             }
             else
             {
@@ -163,6 +176,8 @@ public class PlayerMovement : MonoBehaviour
 
         // orient
         transform.localScale = new Vector3(_facingX, _facingY, 1f);
+        
+            
 
         // move
         transform.Translate(1f * Time.deltaTime * _horizontalVelocity, 1f * Time.deltaTime * _verticalVelocity, 0f);
