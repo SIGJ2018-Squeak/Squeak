74fd76e91a9e958e159adade1c52336477caa946
pacificyost <pacificyost@gmail.com>
2018-11-04 13:11:46 -0800

    Footseps script

---
+++ Squeak/Assets/Scripts/Footsteps.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Footsteps : MonoBehaviour {
+    public List<AudioClip> footstepSFX;
+
+    public void PlayFootstep()
+    {
+        if (footstepSFX.Count > 0)
+        {
+            AudioSource.PlayClipAtPoint(footstepSFX[(int)Random.Range(0, footstepSFX.Count - 1)], transform.position);
+        }
+    }
+}
