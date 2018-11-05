2ef27d1455c91f90254004d02e787de8ad402f36
John <john.bonham@kotisdesign.com>
2018-11-04 16:31:41 -0800

    Adds movement to hand

---
+++ Squeak/Assets/Scripts/HandMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandMovement : MonoBehaviour {
+
+    public float handMoveSpeed;
+    public float moveDuration;
+    public int dir;
+
+    private float _handMovementTimer;
+
+	// Use this for initialization
+	void Start () {
+        dir = 1;
+        _handMovementTimer = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _handMovementTimer += Time.deltaTime;
+
+        if(dir == 1)
+        {
+            if(_handMovementTimer > moveDuration)
+            {
+                dir = -1;
+                _handMovementTimer = 0f;
+            }
+            else
+            {
+                transform.Translate(1f * Time.deltaTime * handMoveSpeed, 0f, 0f);
+            }
+        }
+        else
+        {
+            if (_handMovementTimer > moveDuration)
+            {
+                dir = 1;
+                _handMovementTimer = 0f;
+            }
+            else
+            {
+                //transform.Translate(-1f * Time.deltaTime * handMoveSpeed, 0f, 0f);
+                transform.Translate(-1f * Time.deltaTime * handMoveSpeed, 0f, 0f);
+            }
+        }
+	}
+}
